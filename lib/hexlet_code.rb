04322aa3a2863bce0e58dfe0b8858fa500953034
Andrey Bondar <a.y.bondar@gmail.com>
2021-11-23 22:00:56 +0300

    add Tag class

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
-require_relative "hexlet_code/version"
+require_relative 'hexlet_code/version'
 
+# Simple Form Builder
 module HexletCode
   class Error < StandardError; end
   # Your code goes here...
+
+  # Build html tag
+  class Tag
+    def self.build(tag, html_options = {}, &block)
+      options = html_options.any? ? html_options.map { |key, value| " #{key}=\"#{value}\"" } : ['']
+
+      is_pair_tag = block_given?
+
+      open_tag = "<#{tag}#{options.join('')}>"
+      content = yield block if is_pair_tag
+      closing_tag = "</#{tag}>"
+
+      is_pair_tag ? "#{open_tag}#{content}#{closing_tag}" : open_tag
+    end
+  end
 end
